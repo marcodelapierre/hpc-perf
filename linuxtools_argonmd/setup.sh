d00276d2402ffcccb2468eb00260d6612e5be300
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-05 13:46:40 +0800

    linux tools: added timing for ofast
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- linuxtools_argonmd/setup.sh
+++ linuxtools_argonmd/setup.sh
@@ -3,7 +3,7 @@
 git clone git@github.com:marcodelapierre/argonmd
 cp argonmd/serial_one_source/argonmd.cpp .
 
-for olev in 0 1 2 3 ; do
+for olev in 0 1 2 3 fast ; do
   g++ -std=c++11 -g -O${olev} -o argonmd_o${olev}.x argonmd.cpp
 done
 for olev in 3 fast ; do
--- linuxtools_argonmd/setup_march.sh
+++ linuxtools_argonmd/setup_march.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for olev in 0 1 2 3 ; do
+for olev in 0 1 2 3 fast ; do
   g++ -std=c++11 -g -O${olev} -march=native -o argonmd_o${olev}_march.x argonmd.cpp
 done
 for olev in 3 fast ; do
