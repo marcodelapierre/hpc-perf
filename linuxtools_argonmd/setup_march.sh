fca4b4027dcbd9b6385907b6d8bb076b2fcfc6eb
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-05 13:22:39 +0800

    linux tools: timing for more march cases
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ linuxtools_argonmd/setup_march.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for olev in 0 1 2 3 ; do
+  g++ -std=c++11 -g -O${olev} -march=native -o argonmd_o${olev}_march.x argonmd.cpp
+done
+for olev in 3 fast ; do
+  g++ -std=c++11 -g -O${olev} -march=native -funroll-loops -o argonmd_o${olev}_unroll_march.x argonmd.cpp
+done
