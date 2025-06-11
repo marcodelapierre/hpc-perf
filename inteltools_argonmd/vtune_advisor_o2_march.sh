a9ff95236a1c3822d8c4768ba29814f5f6f19b77
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-11 10:28:30 +0800

    intel argon: adding exps with vtune and advisor
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ inteltools_argonmd/vtune_advisor_o2_march.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module purge
+. /opt/intel/oneapi/setvars.sh
+
+# run-pass-thru options fix the error `SampleCallback: timestamps aren't ascended`
+
+vtune -collect hotspots -run-pass-thru=-timestamp=sys -r ./vtune_o2_march_hotspots -- ./argonmd_o2_march.x &>out_vtune_o2_march_hotspots
+vtune -collect memory-consumption -run-pass-thru=-timestamp=sys -r ./vtune_o2_march_memory_cons -- ./argonmd_o2_march.x &>out_vtune_o2_march_memory_cons
+
+# not available
+#vtune -collect uarch-exploration -run-pass-thru=-timestamp=sys -r ./vtune_o2_march_uarch -- ./argonmd_o2_march.x
+
+#advisor --collect=roofline --project-dir=./adv_o2_march ./argonmd_o2_march.x 16
+advisor --collect survey --run-pass-thru timestamp=sys --project-dir ./adv_o2_march -- ./argonmd_o2_march.x &>out_adv_survey_o2_march
+advisor --collect tripcounts --flop --no-trip-counts --project-dir ./adv_o2_march -- ./argonmd_o2_march.x &>out_adv_tripcounts_o2_march
+
