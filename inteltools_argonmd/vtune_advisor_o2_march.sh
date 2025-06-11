6a809913f8ece15c782f0e45fe323e5fd4e63dd8
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-11 10:30:16 +0800

    update to argon intel vtune script
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- inteltools_argonmd/vtune_advisor_o2_march.sh
+++ inteltools_argonmd/vtune_advisor_o2_march.sh
@@ -9,9 +9,9 @@ vtune -collect hotspots -run-pass-thru=-timestamp=sys -r ./vtune_o2_march_hotspo
 vtune -collect memory-consumption -run-pass-thru=-timestamp=sys -r ./vtune_o2_march_memory_cons -- ./argonmd_o2_march.x &>out_vtune_o2_march_memory_cons
 
 # not available
-#vtune -collect uarch-exploration -run-pass-thru=-timestamp=sys -r ./vtune_o2_march_uarch -- ./argonmd_o2_march.x
+#vtune -collect uarch-exploration -run-pass-thru=-timestamp=sys -r ./vtune_o2_march_uarch -- ./argonmd_o2_march.x &>out_vtune_o2_march_uarch
 
-#advisor --collect=roofline --project-dir=./adv_o2_march ./argonmd_o2_march.x 16
+#advisor --collect=roofline --project-dir=./adv_o2_march_roof ./argonmd_o2_march.x 16 &>out_adv_o2_march_roof
 advisor --collect survey --run-pass-thru timestamp=sys --project-dir ./adv_o2_march -- ./argonmd_o2_march.x &>out_adv_survey_o2_march
 advisor --collect tripcounts --flop --no-trip-counts --project-dir ./adv_o2_march -- ./argonmd_o2_march.x &>out_adv_tripcounts_o2_march
 
