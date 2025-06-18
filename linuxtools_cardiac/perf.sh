02312f27e36caa801bb1c069471fd02cb90b6059
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-18 16:17:58 +0800

    linux cardiac: add note to perf for threads
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- linuxtools_cardiac/perf.sh
+++ linuxtools_cardiac/perf.sh
@@ -14,3 +14,8 @@ perf record -o perf_o2.data ./heart_demo_o2_unroll.x $FLAGS &>out_perf_record_o2
 perf report --stdio -i perf_o2.data &>out_perf_report_o2
 perf script -i perf_o2.data &>out_perf_script_o2
 
+
+# seems like `perf` only supports pthreads, not OpenMP
+#export OMP_NUM_THREADS=2
+#perf record -s -o perf_o2_thr.data ./heart_demo_o2_unroll.x $FLAGS &>out_perf_record_o2_thr
+#perf report -n -T --stdio -i perf_o2_thr.data &>out_perf_report_o2_thr
