adc55cd7d7ecdd9a490f3368a079586d08fbe7ca
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-18 16:38:12 +0800

    gprofng linux: added affinity output
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- linuxtools_cardiac/gprofng.sh
+++ linuxtools_cardiac/gprofng.sh
@@ -13,7 +13,11 @@ gprofng display text -script script.gpng gprofng_o2.er/ &>out_gprofng_o2_analysi
 
 
 export OMP_NUM_THREADS=2
-
 gprofng collect app -O gprofng_o2_thr.er ./heart_demo_o2_unroll.x $FLAGS &>out_gprofng_o2_thr
-
 gprofng display text -script script_threads.gpng gprofng_o2_thr.er/ &>out_gprofng_o2_thr_analysis
+
+# single HW thread core places for a 6 core CPU with subsequent HW threads
+export OMP_PLACES={0:1}:6:2
+export OMP_PROC_BIND=true
+gprofng collect app -O gprofng_o2_thr_aff.er ./heart_demo_o2_unroll.x $FLAGS &>out_gprofng_o2_thr_aff
+gprofng display text -script script_threads.gpng gprofng_o2_thr_aff.er/ &>out_gprofng_o2_thr_aff_analysis
