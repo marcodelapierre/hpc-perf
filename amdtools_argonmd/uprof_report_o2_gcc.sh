9e4fc243434a33f28359730439f6a988bfccee95
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-23 14:45:23 +0800

    amd argon: added script with examples for cli reports
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ amdtools_argonmd/uprof_report_o2_gcc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module purge
+module load zen
+
+# examples of report generation
+# NOTE: -g --detail --inline
+AMDuProfCLI report -g --detail --inline -i dir_amduprof_o2_gcc_hotspots/AMDuProf-argonmd_o2_gcc-Hotspots_Jun-09-2025_15-35-29/ --report-output detail.csv
+
+# useful categories: cpu openmp mpi
+AMDuProfCLI report -g --detail --inline --category cpu -i dir_amduprof_o2_gcc_hotspots/AMDuProf-argonmd_o2_gcc-Hotspots_Jun-09-2025_15-35-29/ --report-output category_cpu.csv
+
+# views: see info --list view-configs
+AMDuProfCLI report -g --detail --inline --view timer -i dir_amduprof_o2_gcc_hotspots/AMDuProf-argonmd_o2_gcc-Hotspots_Jun-09-2025_15-35-29/ --report-output view_timer.csv
+
+AMDuProfCLI report -g --detail --inline --view ipc_assess -i dir_amduprof_o2_gcc_assess/AMDuProf-argonmd_o2_gcc-EBP_Jun-09-2025_15-35-41/ --report-output view_ipc_assess.csv
+
