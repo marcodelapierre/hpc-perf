13a0f135b7ff2d87e42be4db5ba91a8c225b3166
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-23 13:47:36 +0800

    amd cardiac: adding omp/mpi examples with TBP
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- amdtools_cardiac/uprof_collect_report_o2_gcc_omp_mpi.sh
+++ amdtools_cardiac/uprof_collect_report_o2_gcc_omp_mpi.sh
@@ -26,10 +26,29 @@ AMDuProfCLI collect $omp_flags \
   ./heart_demo_o2_unroll_gcc.x $FLAGS &>out_amduprof_o2_unroll_gcc_$type
 AMDuProfCLI report -i dir_amduprof_o2_unroll_gcc_$type/AMD*
 
+# OpenMP trace (GCC) + tbp
+export OMP_NUM_THREADS=2
+type=omp_tbp
+omp_flags="--config tbp --trace openmp --openmp-impl omplib" # GCC
+#omp_flags="--config tbp --trace openmp --openmp-impl ompt --openmp-scope basic" # OMPT eg AOCC
+AMDuProfCLI collect $omp_flags \
+  -o dir_amduprof_o2_unroll_gcc_$type \
+  ./heart_demo_o2_unroll_gcc.x $FLAGS &>out_amduprof_o2_unroll_gcc_$type
+AMDuProfCLI report -i dir_amduprof_o2_unroll_gcc_$type/AMD*
+
 # MPI (OpenMPI)
 export OMP_NUM_THREADS=1
 type=mpi
 mpi_flags="--trace mpi --mpi-impl openmpi"
+mpirun -n 2 AMDuProfCLI collect $mpi_flags \
+  -o dir_amduprof_o2_unroll_gcc_$type \
+  ./heart_demo_o2_unroll_gcc.x $FLAGS &>out_amduprof_o2_unroll_gcc_$type
+AMDuProfCLI report -i dir_amduprof_o2_unroll_gcc_$type/AMD*
+
+# MPI (OpenMPI) + tbp
+export OMP_NUM_THREADS=1
+type=mpi_tbp
+mpi_flags="--config tbp --trace mpi --mpi-impl openmpi"
 mpirun -n 2 AMDuProfCLI collect $mpi_flags \
   -o dir_amduprof_o2_unroll_gcc_$type \
   ./heart_demo_o2_unroll_gcc.x $FLAGS &>out_amduprof_o2_unroll_gcc_$type
