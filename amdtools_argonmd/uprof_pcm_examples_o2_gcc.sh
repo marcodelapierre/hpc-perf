07794bbfe9abc8775abed06c7e998e4d064ac92e
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-25 16:53:29 +0800

    update
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- amdtools_argonmd/uprof_pcm_examples_o2_gcc.sh
+++ amdtools_argonmd/uprof_pcm_examples_o2_gcc.sh
@@ -3,6 +3,6 @@
 module purge
 module load zen
 
-for m in ipc fp dc l1 ; do
+for m in ipc fp dc l1 memory ; do
   AMDuProfPcm -m $m -c core=0 --html -O dir_amduprof_o2_gcc_pcm_$m -- taskset -c 0 ./argonmd_o2_gcc.x 5 20000 &>out_amduprof_o2_gcc_pcm_$m
 done
