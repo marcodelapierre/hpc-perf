70aad93b28ab9f4d977b645ac6b9656bf2bc3fb8
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-06-26 09:47:21 +0800

    minor adds
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- amdtools_argonmd/uprof_topdown_o2_gcc.sh
+++ amdtools_argonmd/uprof_topdown_o2_gcc.sh
@@ -4,4 +4,4 @@ module purge
 module load zen
 
 # pipeline_util metric unsupported on my laptop
-AMDuProfPcm -m pipeline_util -A system -c core=1 -o out_amduprof_o2_gcc_topdown.csv -- ./argonmd_o2_gcc.x
+AMDuProfPcm -m pipeline_util -A system -c core=0 -o out_amduprof_o2_gcc_topdown.csv -- taskset -c 0 ./argonmd_o2_gcc.x
