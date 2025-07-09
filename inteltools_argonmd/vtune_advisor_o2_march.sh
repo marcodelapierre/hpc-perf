e39810c53f214a97c1d2cd507bd546572201409a
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-07-09 15:40:47 +0800

    intel argon: small update to vtune advisor script
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- inteltools_argonmd/vtune_advisor_o2_march.sh
+++ inteltools_argonmd/vtune_advisor_o2_march.sh
@@ -12,7 +12,9 @@ vtune -collect memory-consumption -run-pass-thru=-timestamp=sys -r ./vtune_o2_ma
 # not available
 #vtune -collect uarch-exploration -run-pass-thru=-timestamp=sys -r ./vtune_o2_march_uarch -- ./argonmd_o2_march.x &>out_vtune_o2_march_uarch
 
-#advisor --collect=roofline --project-dir=./adv_o2_march_roof ./argonmd_o2_march.x 16 &>out_adv_o2_march_roof
+# provides non null output in Zen Epyc, null output in Ryzen
+# in any case, no fine-grained vectorisation information
+#advisor --collect=roofline --project-dir=./adv_o2_march_roof ./argonmd_o2_march.x &>out_adv_o2_march_roof
 advisor --collect survey --run-pass-thru timestamp=sys --project-dir ./adv_o2_march -- ./argonmd_o2_march.x &>out_adv_survey_o2_march
 advisor --collect tripcounts --flop --no-trip-counts --project-dir ./adv_o2_march -- ./argonmd_o2_march.x &>out_adv_tripcounts_o2_march
 
